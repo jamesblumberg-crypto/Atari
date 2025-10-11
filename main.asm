65645e12d8704370af1d3176b3db7d112c4f33e9
Jim Blumberg <james.blumberg@gmail.com>
2025-10-11 09:09:16 -0500

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,52 +1,78 @@
-; Jimventure - An Adventure in Atari 8-bit assembly
-; github.com
+; EdVenture - An Adventure in Atari 8-Bit Assembly
+; github.com/edsalisbury/edventure
+; Mission: EdPossible
+; youtube.com/MissionEdPossible
+; Assemble in MADS: mads -l -t main.asm
+; Video 3: Fun with character sets!
 
-    org $2000
+	org $2000
 
-SAVSMC = $0058 ; SCREEN MEMORY ADDRESS
-sdstl  = $0230 ; START OF DISPLAY LIST
+SDLSTL = $0230  ; Display list starting address
+CHBAS = $02f4   ; CHaracter BAse Register
 
-screen = $4000  ; Screen buffer location
+charset = $3c00 ; Character Set
+screen = $4000  ; Screen buffer
 blank8 = $70    ; 8 blank lines
-antic2 = 2      ; ANTIC mode 2 (text mode 0)
-lms = $40       ; Load Memory Start
-jvb = $41       ; Jump to Vertical Blank   
+lms = $40	    ; Load Memory Scan
+jvb = $41	    ; Jump while vertical blank
 
-antic3 = 3      ; ANTIC mode 3 (text mode 1)
-antic4 = 4      ; ANTIC mode 4 (text mode 2)
-antic5 = 5      ; ANTIC mode 5 (text mode 3)
-antic6 = 6      ; ANTIC mode 6 (text mode 4)
-antic7 = 7      ; ANTIC mode 7 (text mode 5)
+antic2 = 2      ; Antic mode 2
+antic5 = 5	    ; Antic mode 5
 
-; Set up the display list and screen memory
-    LDA #<dlist
-    STA sdstl
-    LDA #>dlist
-    STA sdstl+1
- 
- ; main loop
-    LDY #0
-    ; Load the address of the string into the zero page pointer for screen memory
+; Load display list
+	mwa #dlist SDLSTL
 
+; Set up character set
+	mva #>charset CHBAS
+
+	ldx #0
 loop
-    LDA hello,Y
-    sta screen,Y       ; Store address of string in screen memory address
-    INY
-    CPY #12
-    BNE loop
+	mva chars,x charset,x
+	inx
+	cpx #16
+	bne loop
 
-    JMP *               ; Infinite loop
+	ldy #0
+loop2
+	mva scene,y screen,y
+	iny
+	cpy #2
+	bne loop2
 
-; display list
+	jmp *
+
+; Display List
 dlist
-    .byte blank8, blank8, blank8 
-    .byte antic5 + lms, <screen, >screen
-    .byte antic5, antic5, antic5, antic5, antic5, antic5
-    .byte antic5, antic5, antic5, antic5, antic5
-    
-    .byte jvb, <dlist, >dlist
+	.byte blank8, blank8, blank8
+	.byte antic5 + lms, <screen, >screen
+	.byte antic5, antic5, antic5, antic5, antic5, antic5
+	.byte antic5, antic5, antic5, antic5, antic5
+	.byte jvb, <dlist, >dlist
 
+; %00 = 0
+; %01 = 1
+; %10 = 2
+; %11 = 3
 
-; Data
-hello
-    .BYTE "HELLO ATARI!"
+scene
+	.byte " !"
+
+chars
+	.byte %00000000
+	.byte %00000001
+	.byte %00000110
+	.byte %00011011
+	.byte %00011011
+	.byte %00000110
+	.byte %00000001
+	.byte %00000000
+
+	.byte %00000000
+	.byte %01000000
+	.byte %10010000
+	.byte %11100100
+	.byte %11100100
+	.byte %10010000
+	.byte %01000000
+	.byte %00000000
+	
