18b54297ad25b2e69c2757ccd73e37695c244cad
Jim Blumberg <james.blumberg@gmail.com>
2025-10-18 19:24:52 -0500

    Add files via upload

---
+++ main7.asm
@@ -0,0 +1,149 @@
+; EdVenture - An Adventure in Atari 8-Bit Assembly
+; github.com/edsalisbury/edventure
+; Mission: EdPossible
+; youtube.com/MissionEdPossible
+; Assemble in MADS: mads -l -t main.asm
+; Video 7: Player Missile graphics
+
+; ATASCII Table: https://www.atariwiki.org/wiki/attach/Atari%20ATASCII%20Table/ascii_atascii_table.pdf
+; ATASCII 0-31 Screen code 64-95
+; ATASCII 32-95 Screen code 0-63
+; ATASCII 96-127 Screen code 96-127
+
+; NTSC Color Palette: https://atariage.com/forums/uploads/monthly_10_2015/post-6369-0-47505700-1443889945.png
+; PAL Color Palette: https://atariage.com/forums/uploads/monthly_10_2015/post-6369-0-90255700-1443889950.png
+; PMG Memory Map: https://www.atarimagazines.com/compute/issue64/atari_animation.gif
+
+	org $2000
+
+screen   = $4000   ; Screen buffer
+charset  = $5000  ; Character Set
+pmg      = $6000  ; Player Missile Graphics data
+
+    setup_screen()
+    setup_colors()
+    MVA #>charset CHBAS  ; Point to character set
+    display_map()
+    clear_pmg()
+
+	jmp *
+
+    icl 'hardware.asm'
+    icl 'dlist.asm'
+    icl 'gfx.asm'
+    
+* ---------------------------------*
+* Proc: setup_colors               *
+* Sets up colors                   *
+* ---------------------------------*
+.proc setup_colors
+med_gray = $06
+lt_gray = $0a
+green = $c2
+brown = $22
+black = $00
+peach = $2c 
+blue = $80
+    
+    ; character set colors
+    mva #med_gray COLOR0    ; %01
+    mva #lt_gray COLOR1     ; %10
+    mva #green COLOR2       ; %11 normal  
+    mva #brown COLOR3       ; %11 inverse       
+    mva #black COLOR4       ; %00 background
+
+    ; Player missile colors
+    mva #brown PCOLR0
+    mva #peach PCOLR1
+    mva #blue PCOLR2    
+    mva #black PCOLOR3
+    rts
+    .endp
+
+* ------------------------------------*
+* Proc: clear_pmg                     *
+* Clears memory for Player-Missle Gfx *
+* ------------------------------------*
+.proc clear_pmg
+pmg_p0 = pmg + $200
+pmg_p1 = pmg + $280
+pmg_p2 = pmg + $300
+pmg_p3 = pmg + $380
+
+    ldx #$80
+    lda #0
+loop
+    sta pmg_p0,x
+    sta pmg_p1,x
+    sta pmg_p2,x
+    sta pmg_p3,x
+    dex
+    bne loop
+    rts
+    .endp
+
+* ---------------------------------*
+* Proc: load_pmg                   *
+* Load PMG Graphics                *
+* ---------------------------------* 
+.proc load_pmg
+pmg_p0 = pmg + $200
+pmg_p1 = pmg + $280
+pmg_p2 = pmg + $300
+pmg_p3 = pmg + $380
+
+    ldx #0
+loop
+    mva pmgdata,x pmg_p0+64,x
+    mva pmgdata+8,x pmg_p1+64,x
+    mva pmgdata+16,x pmg_p2+64,x
+    mva pmgdata+24,x pmg_p3+64,x
+    inx
+    cpx #$8
+    bne loop
+    rts
+    .endp
+
+
+* ---------------------------------*
+* Proc: display map                *
+* displays the current map         *
+* ---------------------------------*    
+.proc display_map
+    ldy #0
+loop
+	mva map,y screen,y
+    mva map+40,y screen+40,y 
+    mva map+80,y screen+80,y
+    mva map+120,y screen+120,y
+    mva map+160,y screen+160,y
+    mva map+200,y screen+200,y
+    mva map+240,y screen+240,y
+    mva map+280,y screen+280,y
+    mva map+320,y screen+320,y
+    mva map+360,y screen+360,y
+    mva map+400,y screen+400,y
+    mva map+440,y screen+440,y
+
+	iny
+	cpy #40
+	bne loop
+    rts
+
+map
+    .byte 2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3
+	.byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,2,3,6,7,2,3,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,4,5,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,10,11,4,5,2,3
+    .byte 2,3,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,2,3,4,5,4,5,4,5,2,3
+    .byte 2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3
+    .endp
+                  
+
+	
