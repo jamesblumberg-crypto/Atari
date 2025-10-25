d4a115fdbe39686b196a7d7a17531a43ba4d9c83
Jim Blumberg <james.blumberg@gmail.com>
2025-10-25 13:23:37 -0500

    Add files via upload

--- dlist.asm
+++ dlist.asm
@@ -1,8 +1,7 @@
-* ---------------------------------*
-* Proc: set_screen                 *
-* Sets up the display list         *
-* ---------------------------------*
-
+* --------------------------------------- *
+* Proc: setup_screen                      *
+* Sets up the display list for the screen *
+* --------------------------------------- *
 .proc setup_screen
 blank8 = $70    ; 8 blank lines
 lms = $40	    ; Load Memory Scan
@@ -12,14 +11,21 @@ antic2 = 2      ; Antic mode 2
 antic5 = 5	    ; Antic mode 5
 
 	mwa #dlist SDLSTL
-    rts
+	rts
 
 dlist
 	.byte blank8, blank8, blank8
-	.byte antic5 + lms, <screen, >screen
-	.byte antic5, antic5, antic5, antic5, antic5, antic5
-	.byte antic5, antic5, antic5, antic5, antic5
-	.byte jvb, <dlist, >dlist    
-    .endp 
-
-
+	.byte antic5 + lms, $00, $60
+	.byte antic5 + lms, $80, $60
+	.byte antic5 + lms, $00, $61
+	.byte antic5 + lms, $80, $61
+	.byte antic5 + lms, $00, $62
+	.byte antic5 + lms, $80, $62
+	.byte antic5 + lms, $00, $63
+	.byte antic5 + lms, $80, $63
+	.byte antic5 + lms, $00, $64
+	.byte antic5 + lms, $80, $64
+	.byte antic5 + lms, $00, $65
+	.byte antic5 + lms, $80, $65
+	.byte jvb, <dlist, >dlist
+	.endp
--- main.asm
+++ main.asm
@@ -3,82 +3,186 @@
 ; Mission: EdPossible
 ; youtube.com/MissionEdPossible
 ; Assemble in MADS: mads -l -t main.asm
-; Video 3: Fun with character sets!
+; Episode 9: Bigger map!
+
+; ATASCII Table: https://www.atariwiki.org/wiki/attach/Atari%20ATASCII%20Table/ascii_atascii_table.pdf
+; ATASCII 0-31 Screen code 64-95
+; ATASCII 32-95 Screen code 0-63
+; ATASCII 96-127 Screen code 96-127
+
+; NTSC Color Palette: https://atariage.com/forums/uploads/monthly_10_2015/post-6369-0-47505700-1443889945.png
+; PAL Color Palette: https://atariage.com/forums/uploads/monthly_10_2015/post-6369-0-90255700-1443889950.png
+; PMG Memory Map: https://www.atarimagazines.com/compute/issue64/atari_animation.gif
 
 	org $2000
 
-SDLSTL = $0230  ; Display list starting address
-CHBAS = $02f4   ; CHaracter BAse Register
-COLOR0 = $02C4  ; Color for $01
-COLOR1 = $02C5  ; Color for $10
-COLOR2 = $02C6  ; Color for $11 (normal)
-COLOR3 = $02C7  ; Color for $11 (inverse)
-COLOR4 = $02C8  ; Color for %00 (background)
+map     = $3000 ; Map
+charset = $4000 ; Character Set
+pmg     = $5000 ; Player Missile Data
+canvas  = $6000 ; Screen buffer
 
-
-charset = $3c00 ; Character Set
-screen = $4000  ; Screen buffer
-blank8 = $70    ; 8 blank lines
-lms = $40	    ; Load Memory Scan
-jvb = $41	    ; Jump while vertical blank
-
-antic2 = 2      ; Antic mode 2
-antic5 = 5	    ; Antic mode 5
-
-; Load display list
-	mwa #dlist SDLSTL
-
-; Set up character set
+	setup_screen()
+	setup_colors()
 	mva #>charset CHBAS
-
-	ldx #0
-loop
-	mva chars,x charset,x
-	inx
-	cpx #16
-	bne loop
-
-	ldy #0
-loop2
-	mva scene,y screen,y
-	iny
-	cpy #2
-	bne loop2
+	clear_pmg()
+	load_pmg()
+	setup_pmg()
+	copy_map_to_canvas()
 
 	jmp *
 
-; Display List
-dlist
-	.byte blank8, blank8, blank8
-	.byte antic5 + lms, <screen, >screen
-	.byte antic5, antic5, antic5, antic5, antic5, antic5
-	.byte antic5, antic5, antic5, antic5, antic5
-	.byte jvb, <dlist, >dlist
+* --------------------------------------- *
+* Proc: setup_colors                      *
+* Sets up colors                          *
+* --------------------------------------- *
+.proc setup_colors
+med_gray = $06
+lt_gray = $0a
+green = $c2
+brown = $22
+black = $00
+peach = $2c
+blue = $80
 
-; %00 = 0
-; %01 = 1
-; %10 = 2
-; %11 = 3
+	; Character Set Colors
+	mva #med_gray COLOR0 ; %01
+	mva #lt_gray COLOR1  ; %10
+	mva #green COLOR2	 ; %11
+	mva #brown COLOR3    ; %11 (inverse)
+	mva #black COLOR4    ; %00
 
-scene
-	.byte " !"
+	; Player-Missile Colors
+	mva #brown PCOLR0
+	mva #peach PCOLR1
+	mva #blue PCOLR2
+	mva #black PCOLR3
 
-chars
-	.byte %00000000
-	.byte %00000001
-	.byte %00000110
-	.byte %00011011
-	.byte %00011011
-	.byte %00000110
-	.byte %00000001
-	.byte %00000000
+	rts
+	.endp
 
-	.byte %00000000
-	.byte %01000000
-	.byte %10010000
-	.byte %11100100
-	.byte %11100100
-	.byte %10010000
-	.byte %01000000
-	.byte %00000000
+* --------------------------------------- *
+* Proc: clear_pmg                         *
+* Clears memory for Player-Missile Gfx    *
+* --------------------------------------- *
+.proc clear_pmg
+pmg_p0 = pmg + $200
+pmg_p1 = pmg + $280
+pmg_p2 = pmg + $300
+pmg_p3 = pmg + $380
+
+	ldx #$80
+	lda #0
+loop
+	dex
+	sta pmg_p0,x
+	sta pmg_p1,x
+	sta pmg_p2,x
+	sta pmg_p3,x
+	bne loop
+	rts
+	.endp
+
+* --------------------------------------- *
+* Proc: load_pmg                          *
+* Load PMG Graphics                       *
+* --------------------------------------- *
+.proc load_pmg
+pmg_p0 = pmg + $200
+pmg_p1 = pmg + $280
+pmg_p2 = pmg + $300
+pmg_p3 = pmg + $380
+
+	ldx #0
+loop
+	mva pmgdata,x pmg_p0+64,x
+	mva pmgdata+8,x pmg_p1+64,x
+	mva pmgdata+16,x pmg_p2+64,x
+	mva pmgdata+24,x pmg_p3+64,x
+	inx
+	cpx #8
+	bne loop
+	rts
+	.endp
+
+* --------------------------------------- *
+* Proc: setup_pmg                         *
+* Sets up Player-Missile Graphics System  *
+* --------------------------------------- *
+.proc setup_pmg
+	mva #>pmg PMBASE
+	mva #46 SDMCTL ; Single Line resolution
+	mva #3 GRACTL  ; Enable PMG
+	mva #1 GRPRIOR ; Give players priority
+	lda #120
+	sta HPOSP0
+	sta HPOSP1
+	sta HPOSP2
+	sta HPOSP3
+	rts
+	.endp
+
+* --------------------------------------- *
+* Macro: blit_row                         *
+* Copies one line of map to screen        *
+* --------------------------------------- *
+.macro blit_row map, screen
+	ldx #0
+	ldy #0
+loop
+	lda :map,x
+	asl
+	sta :screen,y
+	iny
+	clc
+	adc #1
+	sta :screen,y
+	iny
+	inx
+	cpx #20
+	bne loop
+	.endm
+
+* --------------------------------------- *
+* Proc: copy_map_to_canvas                *
+* Copies map to canvas with interpolation *
+* --------------------------------------- *
+.proc copy_map_to_canvas
+map_ptr = $92
+canvas_ptr = $94
+
+	mwa #map map_ptr
+	mwa #canvas canvas_ptr
+
+	ldy #0
+loop
+	lda (map_ptr),y
+	asl
+	sta (canvas_ptr),y
+
+	inc canvas_ptr
+	bne next
+	inc canvas_ptr+1
+
+next
+	add #1
+	sta (canvas_ptr),y
+	iny
+	bne loop
+
+	inc map_ptr+1
+	inc canvas_ptr+1
+
+	lda map_ptr+1
+	cmp #>(map + $1000)
+	bne loop
+
+	rts
+	.endp
+
+	icl 'hardware.asm'
+	icl 'dlist.asm'
+	icl 'pmgdata.asm'
+	icl 'map.asm'
+	icl 'gfx.asm'
+	
 	
