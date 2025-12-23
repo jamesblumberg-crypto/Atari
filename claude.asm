4a52de8e0f0080c6121e0599331aa970ff9a49ea
Jim Blumberg <232704665+jamesblumberg-crypto@users.noreply.github.com>
2025-12-22 21:41:58 -0600

    deleting old files

--- claude.asm
+++
@@ -1,6 +0,0 @@
-   ORG $3000        ; Start program at $3000
-    
-start:
-    LDA #$46         ; Load color value
-    STA $D01A        ; Store to border color register
-    JMP start        ; Loop foreve
