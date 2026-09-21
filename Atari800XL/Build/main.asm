23a4ab4c3f632d5207c0cdebe79c89fad5702206
Jim Blumberg <james.blumberg@gmail.com>
2026-09-21 13:46:13 -0400

    main.asm: icl stub over main_p0..p4 (Magic Key / KayBee polish)

--- Atari800XL/Build/main.asm
+++ Atari800XL/Build/main.asm
@@ -1,1 +1,8 @@
-@file:///workspace/Atari800XL/Build/main.asm
+; Atari 800XL Assembly Source File
+; Main program file (split into main_p0..p4 for GitHub MCP upload size limits;
+; MADS concatenates via icl -- identical object to the former monolithic main.asm)
+	icl 'main_p0.asm'
+	icl 'main_p1.asm'
+	icl 'main_p2.asm'
+	icl 'main_p3.asm'
+	icl 'main_p4.asm'
